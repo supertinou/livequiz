abd0af931b2cc0893955ee9ac17a02503a3d6eb5
Martin Lagrange <lagrangemartin@gmail.com>
2015-07-18 14:53:52 +0200

    Add uniq acces key and password to quizzes

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,20 @@
 class Quiz < ActiveRecord::Base
 	validates :owner_email, email: true
-	validates :owner_email, :title, presence: true
+	validates :owner_email, :title, :access_key, :access_password, presence: true
+	validates :access_key, :access_password, uniqueness: true, on: :create	
+
+	before_validation :generate_access_key, :generate_access_password, on: :create
+
+	def generate_access_key
+		 begin
+    		self.access_key = SecureRandom.hex(8)
+  		 end while self.class.exists?(access_key: access_key) 
+	end
+
+	def generate_access_password
+		 begin
+    		self.access_password = SecureRandom.hex(8)
+  		 end while self.class.exists?(access_password: access_password)
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150717225943) do
+ActiveRecord::Schema.define(version: 20150718122500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,21 @@ ActiveRecord::Schema.define(version: 20150717225943) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "quizzes", force: :cascade do |t|
+    t.string   "title"
+    t.string   "owner_email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "access_key"
+    t.string   "access_password"
+  end
+
+  create_table "quizzs", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
