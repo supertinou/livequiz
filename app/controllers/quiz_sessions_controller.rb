9ffd3bdde57592bb3ad00eb1ef77b6ad792238f1
Martin Lagrange <lagrangemartin@gmail.com>
2015-07-20 16:00:46 +0200

    Add acces route to participant quiz session

---
+++ app/controllers/quiz_sessions_controller.rb
@@ -0,0 +1,6 @@
+class QuizSessionsController < ApplicationController
+
+	def show
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:show, :edit, :update, :destroy] 
 
+  get 'quiz_sessions/:session_key/:authorization_key/:authorization_password', to: "quiz_sessions#show"
+
   root to: "quizzes#new" 
+
 end
