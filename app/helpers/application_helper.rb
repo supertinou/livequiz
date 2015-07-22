d284980df787a7887f3b8ead1a1fe1f45dbd9531
Martin Lagrange <lagrangemartin@gmail.com>
2015-07-22 22:27:18 +0200

    Refactor quiz session access link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def quiz_session_path(participant)
+		path = "/quiz_sessions/#{participant.session.access_key}/#{participant.authorization_key}/#{participant.authorization_password}"
+		"http://"+ ENV['HOST']+path
+	end
 end
