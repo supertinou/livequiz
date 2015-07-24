b1690f3541d4cc611a6ca616406402a48fa9f4f2
Martin Lagrange <lagrangemartin@gmail.com>
2015-07-24 19:47:42 +0200

    Turn LiveQuiz::PubNub into a singleton

--- lib/live_quiz/pub_nub.rb
+++ lib/live_quiz/pub_nub.rb
@@ -1,17 +1,24 @@
 require "pubnub"
+require 'singleton'
 
 module LiveQuiz
-	module PubNub
-		extend self
-		def client
-			Pubnub.new(
+	class PubNub
+		include Singleton
+		attr_accessor :pubnub
+
+		def initialize()
+			@pubnub = Pubnub.new(
 		      publish_key: ENV['PUBNUB_PUBLISH_KEY'],
 		      subscribe_key: ENV['PUBNUB_SUBSCRIBE_KEY'],
 		      secret_key: ENV['PUBNUB_SECRET_KEY'],
 		      connect_callback: lambda { |msg| Rails.logger.debug "CONNECTED: #{msg.inspect}" },
 		      error_callback: lambda { |msg| raise "Error connecting to PubNub: #{msg.inspect}" },
 		      logger: Rails.logger
-		    )
+		    )	
+		end
+
+		def self.client
+			self.instance.pubnub
 		end
 	end
 end
